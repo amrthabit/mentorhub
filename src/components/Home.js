9f6ed1e11739cae7891f4d228185029918c23a1d
Amr <36488375+Stabnite@users.noreply.github.com>
2023-12-11 02:02:24 -0500

    init

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+// write a flashy "coming soon page" for the website mentorhub.com
+
+import React from "react";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="header">
+        <p>Coming Soon!</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
